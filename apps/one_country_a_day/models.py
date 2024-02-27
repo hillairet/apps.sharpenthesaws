587dcf45b84c19560cdbc3eaaa9c2b55609ce65a
Ant <anthony.hillairet@gmail.com>
2024-02-27 07:54:52 -0800

    Add Country model

--- apps/one_country_a_day/models.py
+++ apps/one_country_a_day/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    alpha_2 = models.CharField(max_length=2, blank=False)
+    alpha_3 = models.CharField(max_length=3, blank=False)
+    name = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name
