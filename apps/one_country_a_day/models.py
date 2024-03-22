07b411dc6e8071207bf923dd298674f1c0faba75
Ant <anthony.hillairet@gmail.com>
2024-03-21 22:59:59 -0700

    Add CountryData model

--- apps/one_country_a_day/models.py
+++ apps/one_country_a_day/models.py
@@ -8,3 +8,15 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CountryData(models.Model):
+    history = models.TextField()
+    flag = models.TextField()
+    cuisine = models.TextField()
+    dish = models.TextField()
+    area = models.TextField()
+    population = models.TextField()
+    fauna = models.TextField()
+    food_production = models.TextField()
+    borders = models.TextField()
